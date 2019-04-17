87d274eff5d78a610505b1c5792396e506a7c0e2
Carlos Fabio <cfabioramos@gmail.com>
2019-04-16 22:52:45 -0300

    Estudo do dia 16/04/2019

--- livraria-orig/src/br/com/caelum/livraria/bean/LivroBean.java
+++ livraria-orig/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,59 +1,58 @@
 package br.com.caelum.livraria.bean;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
-	
-	private Integer autorSelecionado;
-	
-	public Integer getAutorSelecionado() {
-		return autorSelecionado;
+
+	private Integer autorId;
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
 	}
-	
-	public void setAutorSelecionado(Integer autorSelecionado) {
-		this.autorSelecionado = autorSelecionado;
+
+	public Integer getAutorId() {
+		return autorId;
 	}
-	
+
 	public Livro getLivro() {
 		return livro;
 	}
 
-	public void gravar() {
-		System.out.println("Gravando livro " + this.livro.getTitulo());
-
-//		if (livro.getAutores().isEmpty()) {
-//			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
-//		}
-//
-//		new DAO<Livro>(Livro.class).adiciona(this.livro);
+	public List<Livro> getLivros() {
+		return new DAO<Livro>(Livro.class).listaTodos();
 	}
-	
-	public List<Autor> todosAutores() {
+
+	public void gravar() {
+		if (livro.getAutores().isEmpty()) {
+			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+		}
+		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		
+		this.livro = new Livro();
+	}
+
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+	}
+
+	public List<Autor> getAutores() {
 		return new DAO<Autor>(Autor.class).listaTodos();
 	}
-	
-	public Map<String, Integer> getMapAutores() {
-		
-		Map<String, Integer> map = new LinkedHashMap<String, Integer>();
-		
-		map.put("Autor 1", 1);
-		map.put("Autor 2", 2);
-		map.put("Autor 3", 3);
-		map.put("Autor 4", 4);
-		map.put("Autor 5", 5);
-		
-		return map;
+
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
 	}
 
 }
